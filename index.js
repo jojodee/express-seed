d39874bc5ff5f75be104439ad2f38bb84c202059
jojodee <joan@projectcodex.co>
2016-04-23 15:55:31 +0200

    my nodebot code

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
+var five = require("johnny-five");
+
+var board = new five.Board();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -16,16 +19,36 @@ app.use(express.static('public'));
 app.engine('hbs', expressHandlebars({defaultLayout: 'main'}));
 app.set('view engine', 'hbs');
 
+board.on("ready",function(){
+  var led = new five.Led(13);
+
+io.on('connection', function(socket){
+  socket.on('led:on', function(msg){
+    console.log('message: ' + msg);
+    led.on();
+  });
+});
+
 app.get('/', function(req, res){
     res.render('light');
 });
 
+
 app.post('/light', function(req, res){
     console.log(req.body)
     res.redirect('/');
+    var lightStatus = req.body.status;
+
+    if(lightStatus ==="on"){
+      led.on();
+    }
+    else if(lightStatus ==="off"){
+      led.stop().off();
+    }
 });
 
 var port = process.env.port || 3007;
 http.listen(port, function(){
     console.log('running at port :' , port)
+});
 });
